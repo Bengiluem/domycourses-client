9d17982bc9a58b7f9cebfa16288ad2a6a2bdcb38
Benjamin <benjamin.rivard@calldesk.fr>
2021-04-05 02:37:57 +0200

    trailing slash

--- src/state/Cart/api.ts
+++ src/state/Cart/api.ts
@@ -1,5 +1,5 @@
 export async function postCartQuery(urls: string[]) {
-  return fetch("http://localhost:8000/api/recipes", {
+  return fetch("http://localhost:8000/api/recipes/", {
     method: "POST",
     body: JSON.stringify({ recipes: urls }),
     headers: {
